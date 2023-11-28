be9ba14088aebd41abf3de73f3915150da253b56
standprof <67205728+standprof@users.noreply.github.com>
2023-11-28 13:19:27 +0000

    Add navigation buttons in modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import ListCast from './components/ListCast';
-import Modals from './components/Modals';
-import Nav from './components/Nav';
+import React, { useEffect, useState } from "react";
+import ListCast from "./components/ListCast";
+import Modals from "./components/Modals";
+import Nav from "./components/Nav";
 
 function App() {
   const [cast, setCast] = useState([]);
   let [memberInfo, setMemberInfo] = useState(null);
 
   async function fetchCast() {
-    const response = await fetch('cast.json');
+    const response = await fetch("cast.json");
     setCast(await response.json());
   }
 
@@ -18,17 +18,63 @@ function App() {
 
   return (
     <>
-      <Nav cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
+      <Nav
+        cast={cast}
+        onChoice={(info) => {
+          setMemberInfo(info);
+        }}
+      />
       <div className="container">
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
           <h1>Meet the Stargazers</h1>
-          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <ListCast cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
-          {memberInfo && <Modals member={memberInfo} handleClose={() => { setMemberInfo(null) }} />}
+          <p>
+            Members of an <b>intergalactic alliance</b> paving the way for peace
+            and benevolence among all species. They are known for their
+            enthusiasm for science, for their love of fun, and their dedication
+            to education.
+          </p>
+          <ListCast
+            cast={cast}
+            onChoice={(info) => {
+              setMemberInfo(info);
+            }}
+          />
+          {memberInfo && (
+            <Modals
+              member={memberInfo}
+              handleClose={() => {
+                setMemberInfo(null);
+              }}
+              handlePrevious={() => {
+                let currentIndex = cast.findIndex(
+                  (o) => o.name === memberInfo.name
+                );
+
+                let nextMemberIndex = currentIndex - 1;
+                if (nextMemberIndex <= 0) {
+                  nextMemberIndex = 0;
+                }
+
+                setMemberInfo(cast[nextMemberIndex]);
+              }}
+              handleNext={() => {
+                let currentIndex = cast.findIndex(
+                  (o) => o.name === memberInfo.name
+                );
+
+                let nextMemberIndex = currentIndex + 1;
+                if (nextMemberIndex >= cast.length) {
+                  nextMemberIndex = cast.length - 1;
+                }
+
+                setMemberInfo(cast[nextMemberIndex]);
+              }}
+            />
+          )}
         </hgroup>
       </div>
     </>
-  )
+  );
 }
-export default App
+export default App;
--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -1,4 +1,4 @@
-export default ({ member, handleClose }) => {
+export default ({ member, handleClose, handlePrevious, handleNext }) => {
   return (
     <dialog id="modal-member" open>
       <article>
@@ -18,7 +18,15 @@ export default ({ member, handleClose }) => {
               alt={member.name} />
             <hgroup>
               <h1>{member.name}</h1>
-              <p>{member.bio}</p>
+              <p>{member.bio}</p>              
+              <hgroup style={{marginTop:'20px'}}>
+                <p> 
+                <a href="#" style={{marginRight:'10px', width:200}} role="button" className="outline" 
+                  onClick={handlePrevious}>Previous</a>
+                <a href="#" style={{marginRight:'10px', width:200}} role="button" className="outline" 
+                  onClick={handleNext}>Next</a>
+                </p>                  
+              </hgroup>
             </hgroup>
           </div>
         </hgroup>
